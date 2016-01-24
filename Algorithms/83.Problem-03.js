16a813a9833727460889ea7fbb5a01814f225e29
Anuja Verma <anuja.verma@gmail.com>
2016-01-24 09:43:59 -0800

    add solution to algorithm of palindrome

--- Algorithms/83.Problem-03.js
+++ Algorithms/83.Problem-03.js
@@ -7,4 +7,21 @@
 // Example:
 // "A man, a plan, a canal: Panama" is a palindrome.
 // "race a car" is not a palindrome.
+// var string = "A man, a plan, a canal: Panama";
 
+var string = "race a car";
+
+string = string.replace(/\W/g,'').toUpperCase();
+string = string.split("");
+var midString = parseInt(string.length/2);
+var palindrome = true;
+for(var i=0;i<=midString;i++){
+    if(string[i]!=string[(string.length-1)-i]){
+        palindrome = false;
+        //console.log(string[i]+i+"**"+((string.length-1)-i+string[(string.length-1)-i]));
+
+    }
+}
+if (palindrome===true){
+    console.log("Its a Palindrome!!!");
+}
