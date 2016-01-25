5824d962eac427f6a0e7ba222d152de86c58821b
Anuja Verma <anuja.verma@gmail.com>
2016-01-24 22:03:47 -0800

    add solution for algorithm reverse string

--- Algorithms/84.Problem-04.js
+++ Algorithms/84.Problem-04.js
@@ -1,5 +1,17 @@
+/*
 Challenge 1:
 Reverse a String recursively.
 
 Time Complexity: O(n)
 Space Complexity: O(n)
+*/
+
+var string = "anuja";
+
+string = string.split("");
+var revstring=[];
+
+for(var i=0;i<string.length;i++){
+    revstring.unshift(string[i]);
+}
+console.log(revstring.join(""));
