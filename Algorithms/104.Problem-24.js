0e228cb4bcda2e09ecafe7e754bca91f7df9bfd8
Anuja Verma <anuja.verma@gmail.com>
2016-01-31 00:35:08 -0800

    add solved algorithm for Equilibirium

---
+++ Algorithms/104.Problem-24.js
@@ -0,0 +1,16 @@
+ /**********************************************************************
+  *  Problem 1: Bucket Sort                                            *
+  *                                                                    *
+  *  Prompt: Given an unsorted array of numbers which are in the range *
+  *          of 0.0 to 1.0, and are uniformly distributed across the   *
+  *          range, sort the numbers efficiently.                      *
+  *                                                                    *
+  *  Input:  Unsorted array of numbers in range of 0.0 to 1.0          *
+  *  Output: A sorted array                                            *
+  *                                                                    *
+  *  Example: input = [0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434]     *
+  *           output = [0.1234 0.3434 0.565 0.656 0.665 0.897]         *
+  *                                                                    *
+  *  What are the time and auxilliary space complexity?                *
+  *                                                                    *
+  **********************************************************************/
---
+++ Algorithms/105.Problem-25.js
@@ -0,0 +1,18 @@
+/**********************************************************************
+  *  Problem 2: Kth Smallest Element in a Range                        *
+  *                                                                    *
+  *  Prompt: Given an unsorted array of whole integers in the range    *
+  *          1000 to 9000, find the Kth smallest element in linear time*
+  *          The array can have duplicates.                            *
+  *                                                                    *
+  *  Input:  Unsorted array of whole integers in range of 1000 to 9000 *
+  *          Kth smallest element you want to find                     *
+  *                                                                    *
+  *  Output: Kth smalest element in the range                          *
+  *                                                                    *
+  *  Example: array = [1984, 1337, 9000, 8304, 5150, 9000, 8304], k=5  *
+  *           output = 8304                                            *
+  *                                                                    *
+  *  What are the time and auxilliary space complexity?                *
+  *                                                                    *
+  **********************************************************************/
