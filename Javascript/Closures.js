e6f5781e4f5c9a2c71f08f4132658c4d3583e836
Anuja Verma <anuja.verma@gmail.com>
2016-09-28 22:57:49 -0700

    Add notes from Javascript tutorial

--- Javascript/Closures.js
+++ Javascript/Closures.js
@@ -31,4 +31,76 @@ getSubmarineTicket("Anuja");
 // Here is your transportation ticket via the Submarine.
 // Welcome to the Cold Closures Cove, Anuja!
 // => undefined
-//
+
+// #################### Modifying Bound Values #########################
+
+
+function buildCloveTicketMaker(transport){
+  var passengerNumber = 0;
+  return function(name){
+    passengerNumber++;
+    console.log("Here is your transportation ticket via the "+ transport + ".\n" + "Welcome to the Cold Closures Cove, " + name + "!" + "You are passenger #" + passengerNumber + ".");
+  }
+}
+
+var getSubmarineTicket = buildCloveTicketMaker("Submarine");
+getSubmarineTicket("Anuja");
+
+// #################### Common Mistakes #########################
+
+function assignTorpedo (name, passengerArray){
+  var torpedoAssignment;
+  for(var i=0; i<passengerArray.length;i++){
+    if(passengerArray[i] == name){
+      torpedoAssignment = function(){
+        console.log("Ahoy," +  name + "!\n" +
+                    "Man your post at Torpedo #" + (i+1) +"!");
+      };
+    }
+  }
+  return torpedoAssignment;
+}
+
+var subPassenger = ["luke", "Leia", "Han","Chewie","Yoda","R2-D2","Boba"]
+var giveAssignment = assignTorpedo("Chewie",subPassenger);
+
+// Correct way
+
+function assignTorpedo (name, passengerArray){
+
+  for(var i=0; i<passengerArray.length;i++){
+    if(passengerArray[i] == name){
+      return function(){
+        console.log("Ahoy," +  name + "!\n" +
+                    "Man your post at Torpedo #" + (i+1) +"!");
+
+      };
+    }
+  }
+
+}
+
+var subPassenger = ["luke", "Leia", "Han","Chewie","Yoda","R2-D2","Boba"]
+var giveAssignment = assignTorpedo("Chewie",subPassenger);
+
+giveAssignment();
+
+
+// Another Correct Way
+
+function assignTorpedo (passengerArray){
+  return function(name){
+    for(var i=0; i<passengerArray.length;i++){
+    if(passengerArray[i] == name){
+        console.log("Ahoy," +  name + "!\n" +
+                    "Man your post at Torpedo #" + (i+1) +"!");
+
+    }
+  }
+  };
+  }
+
+var subPassenger = ["luke", "Leia", "Han","Chewie","Yoda","R2-D2","Boba"]
+var giveAssignment = assignTorpedo(subPassenger);
+
+giveAssignment("Chewie");
